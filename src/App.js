ee4da241bc4f17fdb9edafa2a16f450c88812d80
Reiner Knudsen <reinerknudsen@me.com>
2023-12-05 17:07:31 +0100

    Push

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
@@ -7,17 +7,70 @@ import Rank from "./components/Rank/Rank";
 import "tachyons";
 import ParticlesBg from "particles-bg";
 
+// Your PAT (Personal Access Token) can be found in the portal under Authentification
+const PAT = "bb06795ea60a45d2a210faee14877836";
+// Specify the correct user_id/app_id pairings
+// Since you're making inferences outside your app's scope
+const USER_ID = "reinerknudsen";
+const APP_ID = "smartbrain";
+// Change these to whatever model and image URL you want to use
+const MODEL_ID = "face-detection";
+const IMAGE_URL = "https://samples.clarifai.com/metro-north.jpg";
+
 function App() {
-  return (
-    <div className="App">
-      <ParticlesBg type="circles" bg={true} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/*<FaceRecognition />} */}
-    </div>
-  );
+  const [input, setInput] = useState("");
+
+  const onInputChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  const onButtonSubmit = () => {
+    console.log("click");
+    // Face Recognition API
+    const raw = JSON.stringify({
+      user_app_id: {
+        user_id: USER_ID,
+        app_id: APP_ID,
+      },
+      inputs: [
+        {
+          data: {
+            image: {
+              url: IMAGE_URL,
+            },
+          },
+        },
+      ],
+    });
+
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization: "Key " + PAT,
+      },
+      body: raw,
+    };
+    fetch(
+      "https://api.clarifai.com/v2/models/" + MODEL_ID + "/outputs",
+      requestOptions
+    )
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
+    return (
+      <div className="App">
+        <ParticlesBg type="circles" bg={true} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+      </div>
+    );
+  };
 }
 
 export default App;
