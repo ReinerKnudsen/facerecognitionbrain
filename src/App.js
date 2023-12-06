71acc006e42a2ff015945908e31af25c16b6fbc3
Reiner Knudsen <reinerknudsen@me.com>
2023-12-06 10:23:46 +0100

    FaceRecognition working

--- src/App.js
+++ src/App.js
@@ -1,31 +1,51 @@
-import React, { useState } from "react";
-import "./App.css";
-import Navigation from "./components/Navigation/Navigation";
-import Logo from "./components/Logo/Logo";
-import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
-import Rank from "./components/Rank/Rank";
-import "tachyons";
-import ParticlesBg from "particles-bg";
+import React, { useState } from 'react';
+import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import 'tachyons';
+import ParticlesBg from 'particles-bg';
 
 // Your PAT (Personal Access Token) can be found in the portal under Authentification
-const PAT = "bb06795ea60a45d2a210faee14877836";
+const PAT = 'bb06795ea60a45d2a210faee14877836';
 // Specify the correct user_id/app_id pairings
 // Since you're making inferences outside your app's scope
-const USER_ID = "reinerknudsen";
-const APP_ID = "smartbrain";
+const USER_ID = 'reinerknudsen';
+const APP_ID = 'smartbrain';
 // Change these to whatever model and image URL you want to use
-const MODEL_ID = "face-detection";
-const IMAGE_URL = "https://samples.clarifai.com/metro-north.jpg";
+const MODEL_ID = 'face-detection';
+//const IMAGE_URL = imageUrl;
 
 function App() {
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+  const [box, setBox] = useState({});
 
   const onInputChange = (event) => {
-    console.log(event.target.value);
+    setInput(event.target.value);
+  };
+
+  const calculateFaceLocation = (data) => {
+    const faceData = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: faceData.left_col * width,
+      topRow: faceData.top_row * height,
+      rightCol: width - faceData.right_col * width,
+      bottomRow: height - faceData.bottom_row * height,
+    };
+  };
+
+  const displayFacebox = (box) => {
+    setBox(box);
   };
 
   const onButtonSubmit = () => {
-    console.log("click");
+    setImageUrl(input);
     // Face Recognition API
     const raw = JSON.stringify({
       user_app_id: {
@@ -36,41 +56,35 @@ function App() {
         {
           data: {
             image: {
-              url: IMAGE_URL,
+              url: imageUrl,
             },
           },
         },
       ],
     });
-
     const requestOptions = {
-      method: "POST",
+      method: 'POST',
       headers: {
-        Accept: "application/json",
-        Authorization: "Key " + PAT,
+        Accept: 'application/json',
+        Authorization: 'Key ' + PAT,
       },
       body: raw,
     };
-    fetch(
-      "https://api.clarifai.com/v2/models/" + MODEL_ID + "/outputs",
-      requestOptions
-    )
-      .then((response) => response.text())
-      .then((result) => console.log(result))
-      .catch((error) => console.log("error", error));
-    return (
-      <div className="App">
-        <ParticlesBg type="circles" bg={true} />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm
-          onInputChange={this.onInputChange}
-          onButtonSubmit={this.onButtonSubmit}
-        />
-      </div>
-    );
+    fetch('https://api.clarifai.com/v2/models/' + MODEL_ID + '/outputs', requestOptions)
+      .then((response) => response.json())
+      .then((result) => displayFacebox(calculateFaceLocation(result)))
+      .catch((error) => console.log('error', error));
   };
+  return (
+    <div className='App'>
+      <ParticlesBg type='circles' bg={true} />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm onInputChange={onInputChange} onButtonSubmit={onButtonSubmit} />
+      <FaceRecognition box={box} imageUrl={imageUrl} />
+    </div>
+  );
 }
 
 export default App;
